72519892b0019adcdfce9a3888cede678be91d79
tmdk2005@gmail.com <tmdk2005@gmail.com>
2024-10-18 13:31:56 +0200

    added gititnore and old kernel state

--- nectarOS/Kernel.cs
+++ nectarOS/Kernel.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Text;
 using Sys = Cosmos.System;
 
-namespace nectarOS
+namespace test_Os
 {
     public class Kernel : Sys.Kernel
     {
-
+        DateTime sysStartTime = DateTime.Now;
+        public static int memorySize = 16;
+        Cosmos.Core.ManagedMemoryBlock memoryBlock = new Cosmos.Core.ManagedMemoryBlock(16);
         protected override void BeforeRun()
         {
             Console.WriteLine("Cosmos booted successfully. Type a line of text to get it echoed back.");
@@ -17,8 +19,49 @@ namespace nectarOS
         {
             Console.Write("Input: ");
             var input = Console.ReadLine();
-            Console.Write("Text typed: ");
-            Console.WriteLine(input);
+            string[] argv = input.Split(' ');
+            switch (argv[0])
+            {
+                case "echo":
+                    {
+                        Console.WriteLine("printing now");
+                        break;
+                    }
+                case "help":
+                    {
+                        Console.WriteLine("Currently Helping");
+                        break;
+                    }
+                case "sysruntime":
+                    {
+                        Console.WriteLine("{0}", getSysRunTime());
+                        break;
+                    }
+                case "save":
+                    {
+
+                        memoryBlock.Write16((uint)Convert.ToInt16(argv[1]), (ushort)Convert.ToInt16(argv[2]));
+                        break;
+                    }
+                case "load":
+                    {
+
+                        Console.WriteLine("Reading: {0}", memoryBlock.Read16((uint)Convert.ToInt16(argv[1])));
+                        break;
+                    }
+                case "exit":
+                    {
+                        Cosmos.System.Power.Shutdown();
+                        break;
+                    }
+            }
         }
+
+        public TimeSpan getSysRunTime()
+        {
+            return DateTime.Now - sysStartTime;
+        }
+
+
     }
 }
