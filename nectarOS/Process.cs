73ae6042d2db6c4a2b1b7230a83d6291cbea7a08
tmdk2005@gmail.com <tmdk2005@gmail.com>
2024-11-04 11:07:30 +0100

    setup first memory manager, shared memory yet to implement

---
+++ nectarOS/Process.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nectarOS
+{
+    internal class Process
+    {
+        private int processID;
+        public Process(MemManager manager) {
+            processID = this.GetHashCode();
+            manager.initializeMemory(processID, 1 , 77);
+            manager.writeMemory(processID,0,Encoding.ASCII.GetBytes("Hello World"));
+            Console.WriteLine(Encoding.ASCII.GetString(manager.readMemory(processID, 0)));
+        }
+    }
+}
